de5f1c016656f660d033e6d944bab98c8d1e4841
JeikerM19 <1001.31533895.ucla@gmail.com>
2024-07-11 10:09:28 -0400

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -4,7 +4,7 @@ import Cl_empresa from "./Cl_empresa.js";
 let art1 = new Cl_articulos(888, 10, 15);
 let art2 = new Cl_articulos(777, 20 , 25 );
 let art3 = new Cl_articulos(999, 15, 25);
-let art4 = new Cl_articulos(333, 25, 35);
+let art4 = new Cl_articulos(666, 25, 35);
 let art5 = new Cl_articulos(111, 50, 70);
 let art6 = new Cl_articulos(333, 40, 50);
 let art7 = new Cl_articulos(444, 80, 100);
